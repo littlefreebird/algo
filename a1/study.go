bb000ba1bd15b11de3fce7123fc57f8d82334b45
liutao <liutao3@erwan365.com>
2021-10-10 21:17:54 +0800

    修改文件夹名

--- a1/study.go
+++ a1/study.go
@@ -1,12 +1,12 @@
+package main
+
+import "fmt"
+
 /*
 给定整数数组nums和一个整数目标值target，在数组中找出和为target的两个数的下标
 输入：nums = [2, 7, 11, 15], target = 9
 输出：[0, 1]
 */
-
-package main
-
-import "fmt"
 
 func f(nums []int, target int) {
 	num2idx := make(map[int]int)
