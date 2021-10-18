fa3721f031454e6fe55a4ea43178149ef6c070f0
liutao <liutao3@erwan365.com>
2021-10-18 23:51:14 +0800

    new algo

---
+++ a4/study.go
@@ -0,0 +1,50 @@
+package study
+/*
+给定一个字符串，找出其中最长回文子串
+输入：s = "abcbx"
+输出："bcb"
+
+输入：s = "a"
+输出："a"
+
+输入：s = "baax"
+输出："aa"
+*/
+
+func f(s string) string {
+	left := 0
+	right := 0
+	max := 0
+	for idx, item := range s {
+		i := idx
+		j := idx
+		for {
+			if i > 0 && s[i - 1] == uint8(item)  {
+				i = i - 1
+			} else {
+				break
+			}
+		}
+		for {
+			if j < len(s) - 1 && s[j + 1] == uint8(item) {
+				j = j + 1
+			} else {
+				break
+			}
+		}
+		for {
+			if i > 0 && j < len(s) - 1 && s[i - 1] == s[j + 1] {
+				i = i - 1
+				j = j + 1
+			} else {
+				break
+			}
+		}
+		if j - i + 1 > max {
+			max = j - i + 1
+			left = i
+			right = j
+		}
+	}
+	return s[left : right + 1]
+}
---
+++ a4/study_test.go
@@ -0,0 +1,32 @@
+package study
+
+import "testing"
+
+func TestF(t *testing.T)  {
+	cases := []struct{
+		name string
+		s string
+	}{
+		{
+			name: "case1",
+			s: "abcd",
+		},
+		{
+			name: "case2",
+			s: "axyxbcd",
+		},
+		{
+			name: "case3",
+			s: "aaaaa",
+		},
+		{
+			name: "case4",
+			s: "xyxmbbbbbbnq",
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Logf("%v\n", f(tt.s))
+		})
+	}
+}
