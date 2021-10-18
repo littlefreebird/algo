759e5015faf5feeda29fb8901135af50243c1ebc
liutao <liutao3@erwan365.com>
2021-10-18 23:53:41 +0800

    modify package name

--- a1/study.go
+++ a1/study.go
@@ -1,12 +1,12 @@
-package main
-
-import "fmt"
+package a1
 
 /*
 给定整数数组nums和一个整数目标值target，在数组中找出和为target的两个数的下标
 输入：nums = [2, 7, 11, 15], target = 9
 输出：[0, 1]
 */
+
+import "fmt"
 
 func f(nums []int, target int) {
 	num2idx := make(map[int]int)
--- a1/study_test.go
+++ a1/study_test.go
@@ -1,4 +1,4 @@
-package main
+package a1
 
 import "testing"
 
