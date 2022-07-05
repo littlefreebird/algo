a648dd65eb62115c77006734102a457476bca019
Tonny Liu <tonnyliu@Tonnys-MacBook-Pro.local>
2022-07-06 00:12:40 +0800

    new algo

---
+++ a33/go.mod
@@ -0,0 +1,3 @@
+module github.com/littlefreebird/algo/a33
+
+go 1.18
---
+++ a33/study.go
@@ -0,0 +1,26 @@
+package a33
+
+//给定一个无重复数字的数组和一个target，在数组中找出所有和为target的数字组合，组合中数字可重复
+
+func f(a []int, target int) [][]int {
+	var p []int
+	var ret [][]int
+	dfs(a, target, 0, p, ret)
+	return ret
+}
+
+func dfs(a []int, target int, idx int, p []int, paths [][]int) {
+	if target < 0 {
+		return
+	}
+	if target == 0 {
+		paths = append(paths, p)
+	}
+	for i := idx; i < len(a); i++ {
+		if a[i] < target {
+			p = append(p, a[i])
+			dfs(a, target-a[i], i, p, paths)
+			p = p[0 : len(p)-1]
+		}
+	}
+}
