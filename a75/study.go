7a6a910b42ed8ef3e651d66f6986a6f3c8c10c02
Tonny Liu <tonnyliu@Tonnys-MacBook-Pro.local>
2022-07-16 15:51:10 +0800

    new algo

--- a75/study.go
+++ a75/study.go
@@ -1,1 +1,28 @@
 package a75
+
+//找出二叉搜索树的第k个结点
+
+type BNode struct {
+	node  int
+	left  *BNode
+	right *BNode
+}
+
+func f(tree *BNode, k int) *BNode {
+	var node *BNode
+	f1(tree, &k, node)
+	return node
+}
+
+func f1(tree *BNode, rest *int, node *BNode) {
+	if tree == nil {
+		return
+	}
+	if *rest == 1 {
+		node = tree
+		return
+	}
+	f1(tree.left, rest, node)
+	*rest = *rest - 1
+	f1(tree.right, rest, node)
+}
