e20c78b43f7ae164b6a5c5cd23d461ecac3675c6
Tonny Liu <tonnyliu@Tonnys-MacBook-Pro.local>
2022-07-16 14:13:37 +0800

    new algo

---
+++ a74/go.mod
@@ -0,0 +1,3 @@
+module github.com/littlefreebird/algo/a74
+
+go 1.18
--- a74/study.go
+++ a74/study.go
@@ -1,1 +1,46 @@
 package a74
+
+//给定一个二叉树和二叉树上的一个结点，找出此给定结点在中序遍历中的上一个结点，二叉树的每个结点除了左右子树指针以外，还有一个指向父结点的指针
+
+type BNode struct {
+	node   int
+	left   *BNode
+	right  *BNode
+	parent *BNode
+}
+
+func f(tree *BNode, node *BNode) *BNode {
+	if tree == nil || node == nil {
+		return nil
+	}
+	if tree == node {
+		if node.left == nil {
+			p := node.parent
+			q := node
+			for {
+				if p == nil {
+					return nil
+				}
+				if p.right == q {
+					return p
+				}
+				q = p
+				p = q.parent
+			}
+		} else {
+			p := tree.left
+			for {
+				if p.left == nil {
+					return p
+				}
+				p = p.left
+			}
+		}
+	}
+	r := f(tree.left, node)
+	if r == nil {
+		return f(tree.right, node)
+	} else {
+		return r
+	}
+}
