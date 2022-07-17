68b3f9a3f76a0f6c073fa4773c0e12fc753320ea
Tonny Liu <tonnyliu@Tonnys-MacBook-Pro.local>
2022-07-17 17:50:14 +0800

    new algo

---
+++ a80/go.mod
@@ -0,0 +1,3 @@
+module github.com/littlefreebird/algo/a80
+
+go 1.18
--- a80/study.go
+++ a80/study.go
@@ -1,1 +1,13 @@
 package a80
+
+//我们可以用2*1的矩阵去覆盖更大的矩阵，请问用n个2*1的矩阵去覆盖一个2*n的大矩阵，有多少种方法
+
+func f(n int) int {
+	if n == 1 {
+		return 1
+	}
+	if n == 2 {
+		return 2
+	}
+	return f(n-1) + f(n-2)
+}
